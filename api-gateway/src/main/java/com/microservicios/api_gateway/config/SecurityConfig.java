972fe09ce7ab7c7148ead671aa595e8b9ea77bbc
DanielJCC <danijocogollo@gmail.com>
2024-05-20 19:27:14 -0500

    fix duplicated dependencies and error with security config in api gateway

--- api-gateway/src/main/java/com/microservicios/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/microservicios/api_gateway/config/SecurityConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.*;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity // Si esto llega a dar algún error comentarlo o quitarlo
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
