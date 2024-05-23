56c4c53594487165b5047a9ebb3a126f6604a082
DanielJCC <danijocogollo@gmail.com>
2024-05-22 19:35:09 -0500

    change static token in Feign communication

--- api-gateway/src/main/java/com/microservicios/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/microservicios/api_gateway/config/SecurityConfig.java
@@ -3,12 +3,10 @@ package com.microservicios.api_gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity // Si esto llega a dar algún error comentarlo o quitarlo
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
--- booking-service/src/main/java/com/microservicios/booking_service/config/FeignConfig.java
+++ booking-service/src/main/java/com/microservicios/booking_service/config/FeignConfig.java
@@ -1,11 +1,9 @@
 package com.microservicios.booking_service.config;
 
-import com.microservicios.booking_service.utils.AuthService;
 import feign.Feign;
 import feign.RequestInterceptor;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
-        return requestTemplate -> {
-            String token = "";
-            requestTemplate.header("Authorization", "Bearer " + token);
-        };
+        return new FeingInterceptor();
     }
     @Bean
     public Feign.Builder feignBuilder(MeterRegistry meterRegistry){
